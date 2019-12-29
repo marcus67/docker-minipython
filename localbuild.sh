890fd2fca367254fbdc468c80c30b51400c85d6e
Marcus Rickert <marcus.rickert@web.de>
2019-12-29 18:19:45 +0100

    * Move tag if neccessary

--- localbuild.sh
+++ localbuild.sh
@@ -1,3 +1,6 @@
-VERSION=0.9
-git tag -a ${VERSION} -m "Version ${VERSION}"
-docker build -t marcusrickert/docker-minipython:${VERSION}  . $*
+#!/usr/bin/env bash
+
+# See https://stackoverflow.com/questions/1404796/how-to-get-the-latest-tag-name-in-current-branch-in-git
+VERSION=$(git describe --abbrev=0)
+
+docker build -t marcusrickert/docker-minipython:${VERSION} .
